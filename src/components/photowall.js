d34cd4feed09f28e83920c7c36eb449ca7fd89df
github username <meingorn@gmail.com>
2017-06-09 12:59:32 -0400

    init commit

---
+++ src/components/photowall.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Photowall extends Component {
+  render() {
+    return (
+      <div className="Photowall">
+
+      </div>
+    );
+  }
+}
+
+export default Photowall;
